d445c61ec57e4523194bba7072074f6c070a3fa9
MatheusHenrique2310 <matheushenriq139@gmail.com>
2024-04-03 09:10:49 -0300

    primeiro projeto dio

--- src/index.js
+++ src/index.js
@@ -1,2 +1,22 @@
-let nome = "MATHEUS"
-console.log (nome)
+let nomeHeroi = "batman"
+let xpHeroi = [
+    ["Ferro", 1000],
+    ["Bronze",1001 && 2000],
+    ["Prata", 2001 && 5000],
+    ["Ouro", 5001 && 7000],
+    ["Platina", 7001 && 8000],
+    ["Ascendente", 8001 && 9000],
+    ["Imortal", 9001 && 10000],
+    ["Radiante", 10001]
+
+
+
+
+
+
+  
+
+
+
+
+
